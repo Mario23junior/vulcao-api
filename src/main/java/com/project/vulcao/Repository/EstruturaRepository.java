90dba613b190799ae79a5ba65f51196f65050cef
Mario23junior <mariojunior3251@gmail.com>
2021-08-10 15:47:59 -0300

    Criando repository da tabela de estrutura

---
+++ src/main/java/com/project/vulcao/Repository/EstruturaRepository.java
@@ -0,0 +1,9 @@
+package com.project.vulcao.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.project.vulcao.Model.Estrutura;
+
+public interface EstruturaRepository extends JpaRepository<Estrutura, Long>{
+
+}
