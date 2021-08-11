16292ac5ef4cb62d7c8665eb6255550297890d1b
Mario23junior <mariojunior3251@gmail.com>
2021-08-11 03:30:06 -0300

    Criando camada de controllerVulcao

---
+++ src/main/java/com/project/vulcao/Controller/ControllerVulcao.java
@@ -0,0 +1,26 @@
+package com.project.vulcao.Controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.project.vulcao.Model.Vulcao;
+import com.project.vulcao.Service.ServiceVulcao;
+
+@RestController
+@RequestMapping("/api/v1/geo/vulcaos")
+public class ControllerVulcao {
+  
+	private ServiceVulcao serviceVulcao;
+	
+	public ControllerVulcao(ServiceVulcao serviceVulcao) {
+		this.serviceVulcao = serviceVulcao;
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Vulcao> volcanoListId(@PathVariable Long id) {
+		return serviceVulcao.listVulcaoById(id);
+	}
+}
