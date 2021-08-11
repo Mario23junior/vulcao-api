a4f605a0abd059bc2e8d227a685eeb7ccdc28c5f
Mario23junior <mariojunior3251@gmail.com>
2021-08-11 18:20:03 -0300

    corrignido bug

--- src/main/java/com/project/vulcao/Controller/ControllerVulcao.java
+++ src/main/java/com/project/vulcao/Controller/ControllerVulcao.java
@@ -9,8 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 import com.project.vulcao.Model.Vulcao;
 import com.project.vulcao.Service.ServiceVulcao;
 
-import io.swagger.annotations.ApiOperation;
-
 @RestController
 @RequestMapping("/api/v1/geo/vulcaos")
 public class ControllerVulcao {
@@ -21,8 +19,7 @@ public class ControllerVulcao {
 		this.serviceVulcao = serviceVulcao;
 	}
 	
-	@ApiOperation(value = "Listando vulcaoes por codigo de indentificação", nickname = "ListandoVulcaoId")
-	@GetMapping("/{id}")
+ 	@GetMapping("/{id}")
 	public ResponseEntity<Vulcao> volcanoListId(@PathVariable Long id) {
 		return serviceVulcao.listVulcaoById(id);
 	}
