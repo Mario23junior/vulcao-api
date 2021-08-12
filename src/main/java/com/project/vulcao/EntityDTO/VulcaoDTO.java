d4a215c92190d11ce89f8576ca302d507e154f59
Mario23junior <mariojunior3251@gmail.com>
2021-08-12 03:39:58 -0300

    listando vulcaoes por id e convertendo entidade em DTO da tabela vulcao

---
+++ src/main/java/com/project/vulcao/EntityDTO/VulcaoDTO.java
@@ -0,0 +1,56 @@
+package com.project.vulcao.EntityDTO;
+
+import java.time.LocalDate;
+
+public class VulcaoDTO {
+
+ 
+	private Boolean ativo;
+
+	private String formadoPor;
+
+	private LocalDate ultimaErupcao;
+
+	private String descricao;
+
+	public VulcaoDTO(Boolean ativo, String formadoPor, LocalDate ultimaErupcao, String descricao) {
+		super();
+ 		this.ativo = ativo;
+		this.formadoPor = formadoPor;
+		this.ultimaErupcao = ultimaErupcao;
+		this.descricao = descricao;
+	}
+ 
+	public Boolean getAtivo() {
+		return ativo;
+	}
+
+	public void setAtivo(Boolean ativo) {
+		this.ativo = ativo;
+	}
+
+	public String getFormadoPor() {
+		return formadoPor;
+	}
+
+	public void setFormadoPor(String formadoPor) {
+		this.formadoPor = formadoPor;
+	}
+
+	public LocalDate getUltimaErupcao() {
+		return ultimaErupcao;
+	}
+
+	public void setUltimaErupcao(LocalDate ultimaErupcao) {
+		this.ultimaErupcao = ultimaErupcao;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+}
--- src/main/java/com/project/vulcao/Service/ServiceVulcao.java
+++ src/main/java/com/project/vulcao/Service/ServiceVulcao.java
@@ -5,23 +5,29 @@ import java.util.Optional;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import com.project.vulcao.EntityDTO.VulcaoDTO;
 import com.project.vulcao.Model.Vulcao;
 import com.project.vulcao.Repository.VulcaoRepository;
-
+import com.project.vulcao.Service.ConvertingEntity.ConvertingEntityVulcao;
+ 
 @Service
 public class ServiceVulcao {
   
 	private VulcaoRepository vulcaoRepository;
-	
+	public ConvertingEntityVulcao convertingEntityVulcao;
+ 	
 	public ServiceVulcao(VulcaoRepository vulcaoRepository) {
 		 this.vulcaoRepository = vulcaoRepository;
-	}
+  	}
 	
 	
-	public ResponseEntity<Vulcao> listVulcaoById (Long id) {
+	public ResponseEntity<VulcaoDTO> listVulcaoById (Long id) {
 		Optional<Vulcao> listId = vulcaoRepository.findById(id);
 		return listId.isPresent()
-				      ? ResponseEntity.ok(listId.get())
+				      ? ResponseEntity.ok(ConvertingEntityVulcao.convertingToDTO(listId.get()))
 				      : ResponseEntity.notFound().build();
 	}
+	
+
 }
+
