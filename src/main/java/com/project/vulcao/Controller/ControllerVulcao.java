bc4d246862f642b66fdfe26761e102d22e9d1d40
Mario23junior <mariojunior3251@gmail.com>
2021-08-14 06:51:52 -0300

    deletando dados do banco de dados

--- src/main/java/com/project/vulcao/Controller/ControllerVulcao.java
+++ src/main/java/com/project/vulcao/Controller/ControllerVulcao.java
@@ -1,6 +1,7 @@
 package com.project.vulcao.Controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -37,15 +38,8 @@ public class ControllerVulcao {
  		return serviceVulcao.updateData(id, vulcao);
    	}
  	
-
+ 	@DeleteMapping("/{id}")
+ 	public ResponseEntity<Vulcao> deleteVulcao(@PathVariable Long id) {
+ 		return serviceVulcao.deleteVulcao(id);
+ 	}
 }
-
-
-
-
-
-
-
-
-
-
