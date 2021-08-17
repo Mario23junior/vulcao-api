e577b087edbfc8b0f322d0ba4e3be0ace0d48ee5
Mario23junior <mariojunior3251@gmail.com>
2021-08-17 11:52:02 -0300

    Criando entidade DTO da entidade Localizacao

---
+++ src/main/java/com/project/vulcao/EntityDTO/LocalizacaoDTO.java
@@ -0,0 +1,48 @@
+package com.project.vulcao.EntityDTO;
+
+public class LocalizacaoDTO {
+   
+	private Long id;
+	private String continente;
+	private String pais;
+	private Double metros;
+	private Double pes;
+	private String cooordenadas;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getContinente() {
+		return continente;
+	}
+	public void setContinente(String continente) {
+		this.continente = continente;
+	}
+	public String getPais() {
+		return pais;
+	}
+	public void setPais(String pais) {
+		this.pais = pais;
+	}
+	public Double getMetros() {
+		return metros;
+	}
+	public void setMetros(Double metros) {
+		this.metros = metros;
+	}
+	public Double getPes() {
+		return pes;
+	}
+	public void setPes(Double pes) {
+		this.pes = pes;
+	}
+	public String getCooordenadas() {
+		return cooordenadas;
+	}
+	public void setCooordenadas(String cooordenadas) {
+		this.cooordenadas = cooordenadas;
+	}
+}
